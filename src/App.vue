05068fd9b99dc2e94ef86d9c5ecf54562a0bffba
carlos <carloshh2354@gmail.com>
2024-12-09 21:09:30 -0600

    primer comit
    primer comit

---
+++ src/App.vue
@@ -0,0 +1,167 @@
+<script>
+
+
+
+</script>
+
+<template>
+  
+    <div class="pagina">
+       
+  <div class="imagen">
+    <img src="../imagen/img1.jpeg">
+  </div>
+    <div class="texto">
+    <h1>Simple Homelette Recipe</h1>
+    <p class="inicio_1">
+   an easy and quick dish,perfect for any meal. this classic omellete combines beatebn egg<br> coked ti perfect,optyonally filled width your choise of cheese,vegetable,or meats
+    </p>
+
+    <div class="lista">
+    <h2>Prepartionn time</h2>
+    <ul>
+      <li><strong>Total:</strong>Approximately 10 minutes</li>
+      <li><strong>Preparation:</strong>5 minutes</li>
+      <li><strong>Cooking:</strong>5minutes</li>
+    </ul>
+    </div>
+
+    <div class="ingredienes">
+      <h2>ingredienes</h2>
+      <ul>
+        <li>2-3 large eggs</li>
+        <li>Sal, to taste</li>
+        <li>Paper, ti taste</li>
+        <li>1 tableSpoon, of buffer or oil</li>
+        <li>Option filling: cheese, diced vegetable, coked</li>
+      </ul>
+
+    </div>
+    <hr class="linea1">
+
+
+    <div class="parrafo_introduccion">
+      <h2 class="titulo2">indtructions</h2>
+      <ol type="parrafo1">
+        <li><strong>Beat the eggs:</strong> In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture
+        </li>
+        <br>
+        <li><strong>Heat the pan:</strong> Place a non-stick frying pan over medium heat and add butter or oil. </li>
+        <br>
+        <li><strong>Cook the omelette:</strong> Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.</li>
+        <br>
+        <li><strong>Add fillings (optional):</strong> When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.</li>
+        <br>
+        <li><strong>Fold and serve:</strong> As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate. </li>
+        <br>
+        <li><strong>Enjoy:</strong> Serve hot, with additional salt and pepper if needed.</li>
+      </ol>
+    </div>
+
+    <hr class="linea1">
+
+    <div class="nutricion">
+      <h2 class="titulo3">Nutrition</h2>
+      <p class="nutricion_title">the table below shows nutritional values serving without adictiional filling</p>
+      <pre>          colorie                                                          <strong clas="str1">277kcal</strong></pre>
+      <hr class="linea1">
+      <pre>          Carbs                                                            <strong>Og</strong></pre>
+      <hr class="linea1">
+      <pre>          Protein                                                          <strong>20g</strong></pre>
+      <hr class="linea1">
+      <pre>          fat                                                                 <strong>22g</strong></pre>
+      
+      <br>
+
+    </div>
+    </div>
+  </div>
+
+  
+
+</template>
+
+<style>
+
+body{
+  background-color: rgb(236, 214, 222);
+  
+}
+
+
+.imagen img{
+max-height: 240px;
+border-radius: 14%;
+padding: 6%;
+
+}
+
+
+
+
+
+.texto, p, h1{
+color: black;
+text-align: left;
+max-width: 89%;
+margin-left: 6%;
+font-family: ;
+}
+
+.parrafo_introduccion{
+  font-size: 85%;
+
+}
+
+.texto h1{
+  font-size: 34px; 
+}
+
+
+
+.pagina{
+  background-color: aliceblue;
+  border-radius: 2%;
+  width: 60%;
+  margin-left: 25%;
+}
+
+.lista{
+  background-color: rgb(243, 229, 233);
+  max-width: 89%;
+  margin-left: 6%;
+  text-align: left;
+}
+div h2{
+  color: tomato;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; 
+}
+
+.linea{
+margin: 10%;
+}
+
+div pre{
+font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+font-size: 70%;
+
+}
+
+.str1{
+  color: red;
+}
+
+.nutricion_title{
+  font-size: 91%;
+  
+
+}
+
+.linea1{
+  width: 100%;
+}
+
+
+
+
+</style>
